3c78c149c7b505fde1e298cdcf63c321f04f8fa7
Charlie <lucharles3@gmail.com>
2021-04-11 08:47:35 -0400

    nextj stoofs

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-// pages
-import Home from './home';
+// dependencies
+import Head from 'next/head';
 
 // layout
 import DefaultLayout from '../layout/defaultLayout';
 
-export default ({
+const Home = ({
 }) => {
   return (
     <DefaultLayout>
-      <Home />  
+      <div className="container">
+        <Head>
+            <title>React Components Template</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <h1>
+            NextJS Starting Template
+        </h1>
+
+        <img className='logo-placeholder' src="/favicon.svg" alt='site-logo' />
+      </div>
     </DefaultLayout>
   );
 }
+
+export default Home;
