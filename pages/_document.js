0bd78ea3f74f039a08f819f04d4daf49170df437
Charlie <lucharles3@gmail.com>
2021-04-13 08:14:09 -0400

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  static getInitialProps( {  renderPage } ) {
+    const page = renderPage();
+
+    return {
+        ...page,
+      };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head >
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
